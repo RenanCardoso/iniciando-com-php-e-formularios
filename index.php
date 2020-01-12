e07719da9546aab4d0c529327b0a1841417376ff
Renan Cardoso <renancardoso1910@gmail.com>
2020-01-12 15:55:04 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="nome">
+        <input type="submit" value="enviar">
+    </form>
+
+<!--    <a href="index.php?name=Renan">Nome</a>-->
+    <a href="?name=Renan">Nome</a> <!-- Pode omitir o nome do arquivo que funcionará normalmente
+
+</body>
+</html>
